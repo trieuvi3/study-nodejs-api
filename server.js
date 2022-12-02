29f726153c24dec01976c68edee94ab9534e1806
trieuvi <trieuvi5@gmail.com>
2022-12-02 16:01:14 +0700

    change file

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const port = 8080;
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>from ERIC & HOI DAN IT </h2>');
-    res.end();
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
